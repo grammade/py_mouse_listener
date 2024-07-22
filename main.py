342722e33fdaf56b8393125c7d1de085a5f5d6fb
NNorman <normanseiro@gmail.com>
2024-07-22 12:41:01 +0700

    add global variable

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from pynput import mouse
 
+left_click_count = 0
+right_click_count = 0
+middle_click_count = 0
+
 def on_click(x, y, button, pressed):
+    global left_click_count, right_click_count, middle_click_count
     if pressed:
         if button == mouse.Button.left:
-            print('M1 pressed')
+            left_click_count += 1
         elif button == mouse.Button.right:
-            print('M2 pressed')
+            right_click_count += 1
         elif button == mouse.Button.middle:
-            print('Middle pressed')
+            middle_click_count += 1
+        print(f'M1: {left_click_count}, M2: {right_click_count}, M3: {middle_click_count}')
 
 # Collect events until released
 with mouse.Listener(on_click=on_click) as listener:
